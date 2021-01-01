71a9be9ae2dc5f646633067a45770d737a0855d8
sameedullah <sameedu786@gmail.com>
2021-01-01 10:40:50 +0500

    created boiler plate

---
+++ server/index.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mangoose from 'mangoose';
+import cors from 'cors';
+/*  The syntax below is old ...but in newer versions of node we are able to to use 'import' syntax as written above, 
+    to enable this ALL we need to do is that goto package.json and add a line below "main" as   "type": "module", Thats It !
+
+    const express = require('express');
+*/
+
