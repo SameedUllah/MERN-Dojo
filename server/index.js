d85379361e33142fa33f7a4d21d9ff704a0d289e
sameedullah <sameedu786@gmail.com>
2021-01-09 11:49:21 +0500

    routine work

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import mangoose from 'mangoose';
+import mongoose from 'mongoose';
 import cors from 'cors';
 /*  The syntax below is old ...but in newer versions of node we are able to to use 'import' syntax as written above, 
     to enable this ALL we need to do is that goto package.json and add a line below "main" as "type": "module", Thats It !
@@ -11,14 +11,15 @@ import cors from 'cors';
 const app = express();
 
 app.use(bodyParser.json({limit: '30mb', extended: true}));
-app.use(bodyParser.urlencoder({limit: '30mb', extended: true}));
+app.use(bodyParser.urlencoded({limit: '30mb', extended: true}));
 app.use(cors());
 
 const CONNECTION_URL = 'mongodb+srv://javascriptmastery:icui4cu2o0@cluster0.ig34v.mongodb.net/<dbname>?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
-    .then(() => app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`)))
-    .catch((error) => console.log(error.message) );
-    
+    .then(() => app.listen(PORT, () => console.log(`Server is running ,on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+mongoose.set('useFindAndModify', false);
 
