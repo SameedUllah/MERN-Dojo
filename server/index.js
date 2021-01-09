a947ac68d0b2a9986a3bbb130adff688a4606676
sameedullah <sameedu786@gmail.com>
2021-01-09 08:32:26 +0500

    routine work

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ import bodyParser from 'body-parser';
 import mangoose from 'mangoose';
 import cors from 'cors';
 /*  The syntax below is old ...but in newer versions of node we are able to to use 'import' syntax as written above, 
-    to enable this ALL we need to do is that goto package.json and add a line below "main" as   "type": "module", Thats It !
+    to enable this ALL we need to do is that goto package.json and add a line below "main" as "type": "module", Thats It !
 
     const express = require('express');
 */
@@ -17,5 +17,8 @@ app.use(cors());
 const CONNECTION_URL = 'mongodb+srv://javascriptmastery:icui4cu2o0@cluster0.ig34v.mongodb.net/<dbname>?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message) );
+    
 
