5a5a0501d01abf232188fc0d7348ef4c39365169
sameedullah <sameedu786@gmail.com>
2021-03-16 07:15:42 +0500

    routine work

--- server/index.js
+++ server/index.js
@@ -14,7 +14,8 @@ app.use(bodyParser.json({limit: '30mb', extended: true}));
 app.use(bodyParser.urlencoded({limit: '30mb', extended: true}));
 app.use(cors());
 
-const CONNECTION_URL = 'mongodb+srv://javascriptmastery:icui4cu2o0@cluster0.ig34v.mongodb.net/<dbname>?retryWrites=true&w=majority';
+// const CONNECTION_URL = 'mongodb+srv://javascriptmastery:icui4cu2o0@cluster0.ig34v.mongodb.net/<dbname>?retryWrites=true&w=majority';
+const CONNECTION_URL = 'mongodb+srv://javascriptmastery:icui4cu2o0@cluster0.ig34v.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
